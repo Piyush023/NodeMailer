3ba5e9037e55be14134e1d73709d97038d6060f3
Piyush023 <piyushkhurana23@gmail.com>
2026-06-02 17:57:05 +0530

    Added a home endpoint.

--- index.js
+++ index.js
@@ -35,4 +35,10 @@ app.post('/send-email', (req, res) => {
   });
 });
 
+// Basic Home Endpoint
+app.get('/', (req, res) => {
+  console.log('Hello World');
+  res.send('Hello World');
+});
+
 app.listen(3010, () => console.log('Server running on port 3010'));
